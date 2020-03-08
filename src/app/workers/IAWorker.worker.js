fae9ec9bb817293dbb571c38cd47515e9bf43975
Theo <theo.lemoine0@gmail.com>
2020-03-09 00:00:09 +0100

    :bug: oups, forgot tu put depth back to 4

--- src/app/workers/IAWorker.worker.js
+++ src/app/workers/IAWorker.worker.js
@@ -2,6 +2,6 @@ import { computeMove } from '../utils/IA'
 
 onmessage = message => {
     const { gameState, player, players, turn } = message.data
-    const move = computeMove(gameState, player, players, 5, 6 - turn)
+    const move = computeMove(gameState, player, players, 4, 6 - turn)
     postMessage(move)
 }
